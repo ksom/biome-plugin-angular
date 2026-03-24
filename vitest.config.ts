2afe811348a52f2144c9704aa6a5afab31eef981
Virgile Vivier <virgilevivier@gmail.com>
2026-03-24 23:27:33 +0100

    fix: format vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,12 +6,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'lcov'],
       include: ['tests/helpers/**/*.ts', 'cli.js', 'index.js'],
-      exclude: [
-        'tests/rules/**',
-        'node_modules/**',
-        'coverage/**',
-        '**/*.d.ts',
-      ],
+      exclude: ['tests/rules/**', 'node_modules/**', 'coverage/**', '**/*.d.ts'],
     },
   },
 });
